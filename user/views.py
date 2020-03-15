278e68ba4dbba1cf12eab6eb50cd9b63de61658b
Theodor Holmsen <theodolh@stud.ntnu.no>
2020-03-15 16:25:49 +0100

    finished profile

--- user/views.py
+++ user/views.py
@@ -42,57 +42,9 @@ def profile(request):
     old_user = request.user
 
     if request.method == 'POST':
-        form = SignUpForm(request.POST) #finner forrige verdier || henter nye verdier
+        form = SignUpForm(request.POST) 
         if form.is_valid():
-           
-           #Dette jeg prøver på nå, at hele form-et skal endres, ikke bare enkelt fields
-
-            #new_user = form.save()
-            #old_user = models.update_user_profile(old_user, new_user,created, **kwargs)
-            #form = form.cleaned_data(new_user) #ny profil med "renset verdier', konvertert til riktige verdier
-            #form = SignUpForm(a) #ny form med med ny verdier i gammel form
-            #form.save()                         #lagre de nye verdiene
-
-
-
-
-
-
-
-            #---------------------------------------------------------------
-            #Her skal logikken være riktig. Her har jeg valgt bare noen fields som skal ta inn verdi og kunne byttes, ikke alle som var i sign-up. 
-            #Her tar hver variabel/field inn en ny verdi og "cleaner" den forrige verdien. Deretter skal den nye verdien lagres. 2 setninger per field. Problemet her er save().
-            #Hvis save() fikses for integer verdier så er vi ferdig. Da kan vi kjøre samme logikk for alle mulige fields, men kan også bare ta med de under. Jeg får ikke fikset save()
-
-
-            #.save() kalles på user objektet etter at du er ferdig med å sette parameterene. Se her for mer informasjon:  https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/#the-save-method
-            
-            #user.profile.first_name = form.cleaned_data.get('first_name') #hnavnet til feltet i forms.py 
-            #user.profile.first_name.save()
-            #user.profile.last_name = form.cleaned_data.get('last_name')
-            #user.profile.last_name.save()
-            #user.profile.email = form.cleaned_data.get('email')
-            #user.profile.email.save()
-            #user.profile.email_confirmation = form.cleaned_data.get('email_confirmation')
-            #user.profile.email_confirmation.save()
-            #user.profile.city = form.cleaned_data.get('city')
-            #user.profile.city.save()
-            #user.profile.postal_code = form.cleaned_data.get('postal_code')
-            #user.profile.postal_code.save()
-
-            #---------------------------------------------------------------
-            #Alternativ versjon av over:
-
-            #data = request.POST.copy()
-            #data['username'] = form.cleaned_data.get('username')
-            #new_form = SignUpForm(data) 
-            #if new_form.is_valid():
-                #new_form.save() 
-
-
-            #----------------------------------------------------------------
-            #Skaper bare en ny bruker:
-            
+          
             
             user = form.save()
             user.refresh_from_db()
@@ -104,22 +56,12 @@ def profile(request):
             user.save()
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=user.username, password=raw_password)
-            
-            a = form.cleaned_data.get('username')
-            a = int(a)
-            for field in a.items():     #prøver å dele opp pga mengden, men items() kan ikke brukes for str, men kan ikke konverte til int heller...
-                print(field)
-
-                b = User.objects.get(a)
-                #b = b.split('') 
-                b.delete()
-            
-            
-            #request.user.get(username).delete()
+    
+            old_user.delete()
             
         
             from django.contrib import messages
-            messages.success(request, 'Your account information has been updated')
+            messages.success(request, 'Your account has been reneewed. Sign in to continue')
             return redirect('home')
     else:
         form = SignUpForm()
